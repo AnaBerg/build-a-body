b3dcd2b5e069b6245aeed8f0e824aae255136906
Ana Berg <ana_berg@trimble.com>
2024-04-20 18:07:22 -0300

    feat: testinf config

---
+++ src/app/page.test.tsx
@@ -0,0 +1,14 @@
+import { screen, render } from '@testing-library/react';
+import { expect, describe, it } from 'bun:test';
+
+import Page from './page';
+
+describe('Page', () => {
+  it('should render the page', async () => {
+    render(<Page />);
+
+    const text = await screen.findByText('Hello World');
+
+    expect(text).toBeTruthy();
+  });
+});
