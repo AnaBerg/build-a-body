1e67c3fabed2d6ec5eb39efe06d9cf0a4b70b491
Ana Berg <ana.berg99@gmail.com>
2024-04-23 16:29:05 -0300

    feat: exercise tables (#9)
    Co-authored-by: Ana Berg <ana_berg@trimble.com>

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -5,6 +5,7 @@ import {
   uuid,
   timestamp,
   varchar,
+  integer,
 } from 'drizzle-orm/pg-core';
 
 const createTable = pgTableCreator((name) => `build_a_body_${name}`);
@@ -12,8 +13,6 @@ const createTable = pgTableCreator((name) => `build_a_body_${name}`);
 export const splits = createTable('splits', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: varchar('name', { length: 256 }).notNull(),
-  splitDays: varchar('split_days', { length: 256 }).array().notNull(),
-  exercises: varchar('exerercises', { length: 256 }).array().notNull(),
   userId: varchar('user_id', { length: 256 }).notNull(),
   createdAt: timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
@@ -28,6 +27,57 @@ export const exercises = createTable('exercises', {
   name: varchar('name', { length: 256 }).notNull().unique(),
   description: varchar('description', { length: 256 }),
   muscleGroup: varchar('muscle_group', { length: 256 }).notNull(),
+  createdAt: timestamp('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp('updatedAt')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export const splitDays = createTable('split_days', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  exercises: uuid('exercises')
+    .array()
+    .notNull()
+    .references(() => exercises.id),
+  splitId: uuid('user_id').notNull(),
+  userId: varchar('user_id', { length: 256 }).notNull(),
+  createdAt: timestamp('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp('updatedAt')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export const workouts = createTable('workouts', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  date: timestamp('date').notNull(),
+  splitDayId: uuid('split_day_id')
+    .notNull()
+    .references(() => splitDays.id),
+  userId: varchar('user_id', { length: 256 }).notNull(),
+  createdAt: timestamp('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp('updatedAt')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export const exercisesPerformed = createTable('exercises_performed', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  reps: integer('weight').notNull(),
+  weight: integer('weight').notNull(),
+  exerciseId: uuid('exercise_id')
+    .notNull()
+    .references(() => exercises.id),
+  workoutId: uuid('workout_id')
+    .notNull()
+    .references(() => workouts.id),
+  userId: varchar('user_id', { length: 256 }).notNull(),
   createdAt: timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
