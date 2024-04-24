08583003afbb238edd37f0a60cae3f536c1980a0
Ana Berg <ana.berg99@gmail.com>
2024-04-23 21:25:28 -0300

    feat: split page (#10)
    Co-authored-by: Ana Berg <ana_berg@trimble.com>

--- src/app/exercise/split/add/page.tsx
+++ src/app/exercise/split/add/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { getExercisesForAccordion } from '@/server/queries';
 import AddFormContainer from './_components/FormContainer';
 
@@ -5,6 +6,10 @@ const AddPage: React.FC = async () => {
   const accordions = await getExercisesForAccordion();
 
   return <AddFormContainer accordions={accordions} />;
+=======
+const AddPage: React.FC = () => {
+  return <>AddPage</>;
+>>>>>>> 75c1a6d (feat: split page (#10))
 };
 
 export default AddPage;
