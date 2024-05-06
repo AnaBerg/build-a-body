437d8fbba903be677c4c20d62a064c10607e7627
Ana Berg <ana.berg99@gmail.com>
2024-05-06 15:42:34 -0300

    feat: pwa config (#26)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import configPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+};
+
+const withPWA = configPWA({
+  dest: 'public',
+  disable:
+    process.env.NODE_ENV === 'development' ? process.env.DISABLE_PWA : false,
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
